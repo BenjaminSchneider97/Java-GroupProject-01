764f9630ea8b041bab34dbb3897edadaf3a49536
Simon Blaha <s.blaha@live.at>
2019-05-22 11:54:07 +0200

    create: hashmap and arraylist for grading system

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,27 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        
+
+        Subjects math = new Subjects("Math");
+        Subjects german = new Subjects("German");
+
+        ArrayList<Subjects> subjects_1a = new ArrayList<>();
+        subjects_1a.add(math);
+        subjects_1a.add(german);
+
+        Class a1 = new Class("1A", subjects_1a);
+        Students mark = new Students("Mark", "Forster", "Marry", new Clas);
+
+
+
+        HashMap<String, Integer> subject_math = new HashMap<>();
+
+        subject_math.put(mark.getStudentFirstName(), 30);
+
+
     }
 }
