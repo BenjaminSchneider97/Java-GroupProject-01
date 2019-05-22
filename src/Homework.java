44d0bbc74ca9c55f74a786828297bc0a6df760ce
Simon Blaha <s.blaha@live.at>
2019-05-22 10:41:42 +0200

    edit: homework class, remove class object

--- src/Homework.java
+++ src/Homework.java
@@ -1,8 +1,29 @@
 public class Homework {
 
-
+    private Class classes;
+    private Subjects subject;
 
     public Homework() {
 
+        this.classes = classes;
+        this.subject = subject;
+    }
+
+    // Getter
+    public Class getClasses() {
+        return classes;
+    }
+
+    public Subjects getSubject() {
+        return subject;
+    }
+
+    // Setter
+    public void setClasses(Class classes) {
+        this.classes = classes;
+    }
+
+    public void setSubject(Subjects subject) {
+        this.subject = subject;
     }
 }
