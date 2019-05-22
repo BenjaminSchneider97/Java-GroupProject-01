3f8f2904e5143fc1402e2956c89f46d3e4f5b926
Benjamin Schneider <benjamin.schneider.97@gmail.com>
2019-05-22 10:16:39 +0200

    add properties for school and student

--- src/School.java
+++ src/School.java
@@ -1,4 +1,26 @@
 public class School {
 
-    public School()
+    private String schoolName;
+    private String schoolAddress;
+
+    public School(String schoolName, String schoolAddress){
+        this.schoolName = schoolName;
+        this.schoolAddress = schoolAddress;
+    }
+
+    public String getSchoolName(){
+        return this.schoolName;
+    }
+
+    public void setSchoolName(String schoolName){
+        this.schoolName = schoolName;
+    }
+
+    public String getSchoolAddress(){
+        return this.schoolAddress;
+    }
+
+    public void setSchoolAddress(String schoolAddress){
+        this.schoolAddress = schoolAddress;
+    }
 }
